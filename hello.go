4706a734afc03d4fb6a9f4527fc283e7f0b9da02
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-05-18 13:29:32 -0300

    add configs

--- hello.go
+++ hello.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"anglesson/greetings"
 
 	"rsc.io/quote"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	http.Handle('/', )
+	fmt.Println(messages)
 	fmt.Println(quote.Go())
+}
+
+func Hello(w *http.ResponseWriter, r http.Request) {
+	w.
 }
